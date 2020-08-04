8630b982296e56a9021f270bab710c6237d0506e
Mayko Douglas <maykodouglasbr@outlook.com>
2020-08-03 21:19:09 -0300

    Update

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/20783069?s=460&u=5e23cf29d7cc2f40d8a0b0f6988a548a12b5640a&v=4"
+          alt="Mayko Douglas"
+        />
+        <div>
+          <strong>Mayko Douglas</strong>
+          <span>Matemática</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/Hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
