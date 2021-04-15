3a0e734e9391277da215c46b696a5fd4693b4bee
Mayko Douglas <maykodouglasbr@outlook.com>
2021-04-14 21:02:00 -0300

    Atualização

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://apidevcond.herokuapp.com/api";
+const baseUrl = "https://apiatletica.herokuapp.com/api";
 // const baseUrl = "http://127.0.0.1:8000/api";
 
 const request = async (method, endpoint, params, token = null) => {
